204ffc1ee9000e93b45082ee2e695512f93c6311
sariyarawaekklang <sariyarawaekklang@gmail.com>
2021-12-02 18:51:33 -0600

    testing

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -10,4 +10,21 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a Github username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
